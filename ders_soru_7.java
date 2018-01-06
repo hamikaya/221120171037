d6f9c825985f5d7e3cf6ba5c3a86732789799a78
Hami Kaya <hami6106@gmail.com>
2018-01-06 18:55:13 +0300

    Update ders_soru_7.java

--- ders_soru_7.java
+++ ders_soru_7.java
@@ -4,19 +4,19 @@ public class ders_soru_7 {
 
 	public static void main(String[] args) {
 		Scanner giris = new Scanner(System.in);
-		System.out.print("Sayý Girin: ");
+		System.out.print("SayÄ± Girin: ");
 		int sayi = giris.nextInt();
 		if (String.valueOf(sayi).length()<=4) {
 			for (int i = 1; i!=sayi;) {
 				if (sayi%i==0) {
 					int sonuc = sayi/i;
-					System.out.println(sayi+" sayýsýnýn "+i+" sayýsýna bölümü: "+sonuc);
+					System.out.println(sayi+" sayÄ±sÄ±nÄ±n "+i+" sayÄ±sÄ±na bÃ¶lÃ¼mÃ¼: "+sonuc);
 				}
 				i++;
 			}
 		}
 		else {
-			System.out.print("En fazla 4 basamaklý bir sayý girebilirsiniz.");
+			System.out.print("En fazla 4 basamaklÄ± bir sayÄ± girebilirsiniz.");
 		}
 	}
 
