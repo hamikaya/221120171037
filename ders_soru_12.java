686b7773680c0bc14cea80993d57c16613608fc4
Hami Kaya <hami6106@gmail.com>
2018-01-06 23:21:45 +0300

    Update ders_soru_12.java

--- ders_soru_12.java
+++ ders_soru_12.java
@@ -4,15 +4,15 @@ public class ders_soru_12 {
 
 	public static void main(String[] args) {
 		Scanner giris = new Scanner(System.in);
-		System.out.print("Sayı Girin: ");
+		System.out.print("SayÄ± Girin: ");
 		int sayi1 = giris.nextInt();
-		System.out.print("Sayı Girin: ");
+		System.out.print("SayÄ± Girin: ");
 		int sayi2 = giris.nextInt();
-		System.out.print("Sayı Girin: ");
+		System.out.print("SayÄ± Girin: ");
 		int sayi3 = giris.nextInt();
 		int sonuc1 = Math.max(sayi1, sayi2);
 		int sonuc2 = Math.max(sonuc1, sayi3);
-		System.out.print("Girdiğiniz sayıların en büyüğü: "+sonuc2);
+		System.out.print("GirdiÄŸiniz sayÄ±larÄ±n en bÃ¼yÃ¼ÄŸÃ¼: "+sonuc2);
 	}
 
 }
