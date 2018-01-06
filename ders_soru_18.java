f7ac831ae46e1f86c8609c0c4f76cd0efba8ed14
Hami Kaya <hami6106@gmail.com>
2018-01-07 00:56:26 +0300

    Add files via upload

---
+++ ders_soru_18.java
@@ -0,0 +1,13 @@
+package denemeler;
+import java.util.*;
+public class ders_soru_18 {
+
+	public static void main(String[] args) {
+		Scanner giris = new Scanner(System.in);
+		System.out.print("Celcius deðerini girin: ");
+		double sayi = giris.nextDouble();
+		double fahrenheit = sayi*1.8+32;
+		System.out.print(sayi+" Celcius, "+fahrenheit+" Fahrenheit'e eþittir.");
+	}
+
+}
