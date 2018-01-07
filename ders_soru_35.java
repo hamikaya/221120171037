538242df7df2cff2a21926d39407910d87d1fc12
Hami Kaya <hami6106@gmail.com>
2018-01-07 14:32:30 +0300

    Update ders_soru_35.java

--- ders_soru_35.java
+++ ders_soru_35.java
@@ -4,11 +4,11 @@ public class ders_soru_35 {
 
 	public static void main(String[] args) {
 		Scanner giris = new Scanner(System.in);
-		System.out.print("Sayý Girin: ");
+		System.out.print("SayÄ± Girin: ");
 		int sayi = giris.nextInt();
 		String sonuc1 = String.valueOf(sayi);
 		String sonuc2 = sonuc1.replace(" ", "");
-		System.out.print("Girdiðiniz sayý "+sonuc2.length()+" basamaklý.");
+		System.out.print("GirdiÄŸiniz sayÄ± "+sonuc2.length()+" basamaklÄ±.");
 	}
 
 }
