52beb0d578a39b9c13319d867df7666a50079e35
Hami Kaya <hami6106@gmail.com>
2018-01-07 12:32:38 +0300

    Add files via upload

---
+++ ders_soru_24.java
@@ -0,0 +1,13 @@
+package denemeler;
+import java.util.*;
+public class ders_soru_24 {
+
+	public static void main(String[] args) {
+		Scanner giris = new Scanner(System.in);
+		System.out.print("Sayý Girin: ");
+		int sayi = giris.nextInt();
+        String binary = Integer.toBinaryString(sayi);
+        System.out.print("Girdiðiniz sayýnýn ikilik sistemindeki karþýlýðý: "+binary);
+	}
+
+}
