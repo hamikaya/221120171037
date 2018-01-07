5ed61708480baf6e221581b38b959158ba22f2d8
Hami Kaya <hami6106@gmail.com>
2018-01-07 15:48:18 +0300

    Add files via upload

---
+++ ders_soru_28.java
@@ -0,0 +1,27 @@
+package denemeler;
+import java.util.*;
+public class ders_soru_28 {
+
+	public static void main(String[] args) {
+		Scanner giris = new Scanner(System.in);
+		System.out.print("Sayý Girin: ");
+		int sayi1 = giris.nextInt();
+		int sayac = 0;
+		String sayi = Integer.toString(sayi1);
+		for (int i = sayi.length()-1; i>=0; i--) {
+			for (int a = 0; a<sayi.length(); a++) {
+				if (sayi.charAt(a)==sayi.charAt(i)) {
+					sayac++;
+					System.out.println(sayac);
+				}
+			}
+		}
+		if (sayac/sayi.length()==sayi.length() || sayac!=sayi.length()) {
+			System.out.print(sayi1+" sayýsý palindromdur.");
+		}
+		else {
+			System.out.print(sayi1+" sayýsý palindrom deðildir.");
+		}
+	}
+
+}
