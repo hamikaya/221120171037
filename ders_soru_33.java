d04a240a9cb912868eb7a4fef7d9f168f79f5b88
Hami Kaya <hami6106@gmail.com>
2018-01-07 14:11:14 +0300

    Rename ders_soru_32.java to ders_soru_33.java

--- ders_soru_33.java
+++ ders_soru_33.java
@@ -4,13 +4,13 @@ public class ders_soru_32 {
 
 	public static void main(String[] args) {
 		Scanner giris = new Scanner(System.in);
-		System.out.print("Sayý Girin: ");
+		System.out.print("SayÄ± Girin: ");
 		int sayi = giris.nextInt();
 		if (50<=sayi) {
-			System.out.print("Girilen sayý 50'den büyük. Sayýnýn yüzde 10'u: "+sayi*0.10);
+			System.out.print("Girilen sayÄ± 50'den bÃ¼yÃ¼k. SayÄ±nÄ±n yÃ¼zde 10'u: "+sayi*0.10);
 		}
 		else {
-			System.out.print("Girilen sayý 50'den küçük. Sayýnýn yüzde 25'i: "+sayi*0.25);
+			System.out.print("Girilen sayÄ± 50'den kÃ¼Ã§Ã¼k. SayÄ±nÄ±n yÃ¼zde 25'i: "+sayi*0.25);
 		}
 	}
 
