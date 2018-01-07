bb6d08e13734bb7abaeee9013eaf1aeba623bf2e
Hami Kaya <hami6106@gmail.com>
2018-01-07 13:40:56 +0300

    Update ders_soru_30.java

--- ders_soru_30.java
+++ ders_soru_30.java
@@ -4,22 +4,22 @@ public class ders_soru_30 {
 
 	public static void main(String[] args) {
 		Scanner giris = new Scanner(System.in);
-		System.out.print("Cümle Girin: ");
+		System.out.print("CÃ¼mle Girin: ");
 		String cumlee = giris.nextLine();
 		String cumle = " "+cumlee+" ";
 		String cumle1 = cumle.replace(" ", "");
 		int cu = cumle1.length();
-		String ok = "!'^+%&/()=?_*-,.1234567890>£#$½6{[]}|";
+		String ok = "!'^+%&/()=?_*-,.1234567890>Â£#$Â½6{[]}|";
 		int sayac = cu;
 		for (int i = 0; i<=cu+1; i++) {
 			for (int a = 0; a<=ok.length()-1; a++) {
 				if (ok.charAt(a)==cumle.charAt(i)) {
 					sayac--;
-					System.out.println("Özel karakter tespit edildi: "+ok.charAt(a));
+					System.out.println("Ã–zel karakter tespit edildi: "+ok.charAt(a));
 				}
 			}
 		}
-		System.out.print("Girdiðiniz cümledeki harf sayýsý: "+sayac);
+		System.out.print("GirdiÄŸiniz cÃ¼mledeki harf sayÄ±sÄ±: "+sayac);
 	}
 
 }
