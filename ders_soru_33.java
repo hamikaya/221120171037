23af0237e6b0a556e04c2f25e75ea38099fccc66
Hami Kaya <hami6106@gmail.com>
2018-01-07 14:07:48 +0300

    Add files via upload

---
+++ ders_soru_33.java
@@ -0,0 +1,17 @@
+package denemeler;
+import java.util.*;
+public class ders_soru_33 {
+
+	public static void main(String[] args) {
+		Scanner giris = new Scanner(System.in);
+		System.out.print("Üssü hesaplanacak sayýyý girin: ");
+		int sayi = giris.nextInt();
+		System.out.print("Üssü girin: ");
+		int us = giris.nextInt();
+		for (int i = 1; i<us; i++) {
+			sayi*=sayi;
+			System.out.print("Sonuç: "+sayi);
+		}
+	}
+
+}
